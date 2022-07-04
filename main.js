da9438c72b3412f47a0b0584c9ab1892e3b5e09a
Andarov <bekturandarob678@gmail.com>
2022-07-04 14:36:29 +0500

    Add team section

---
+++ main.js
@@ -0,0 +1,26 @@
+const elsTeamBtn = document.querySelectorAll(".team-js-btn");
+const elsTeamContent = document.querySelectorAll(".team__item-content");
+const elsTeamAbout = document.querySelectorAll(".team__item-about");
+
+elsTeamBtn.forEach(function (elTeamBtn) {
+    elTeamBtn.addEventListener("click", function () {
+        elsTeamContent.forEach(function (elTeamContent) {
+            elTeamContent.classList.add("team__item-content--none")
+        });
+        elsTeamAbout.forEach(function (elTeamAbout) {
+            elTeamAbout.classList.remove("team__about--none")
+        });
+    });
+});
+
+const elsTeamBack = document.querySelectorAll(".team-js-back");
+elsTeamBack.forEach(function (elTeamBack) {
+    elTeamBack.addEventListener("click", function () {
+        elsTeamAbout.forEach(function (elTeamAbout) {
+            elTeamAbout.classList.add("team__about--none")
+        });
+        elsTeamContent.forEach(function (elTeamContent) {
+            elTeamContent.classList.remove("team__item-content--none")
+        });
+    });
+});
