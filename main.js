56c407edb3bb4f9664247fc97d135ba34281776c
Andarov <bekturandarob678@gmail.com>
2022-07-05 16:03:41 +0500

    Change javascript code

--- main.js
+++ main.js
@@ -10,13 +10,9 @@ elsTeamBtn.forEach(function(item, index) {
 });
 
 const elsTeamBack = document.querySelectorAll(".team-js-back");
-elsTeamBack.forEach(function (elTeamBack) {
-    elTeamBack.addEventListener("click", function () {
-        elsTeamAbout.forEach(function (elTeamAbout) {
-            elTeamAbout.classList.add("team__about--none")
-        });
-        elsTeamContent.forEach(function (elTeamContent) {
-            elTeamContent.classList.remove("team__item-content--none")
-        });
+elsTeamBack.forEach(function (item, index) {
+    item.addEventListener("click", function () {
+        elsTeamContent[index].classList.toggle('team__item-content--none')
+        elsTeamAbout[index].classList.toggle('team__about--none')
     });
 });
